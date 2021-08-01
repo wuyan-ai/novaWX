2e8afccf728100344f9a493b3698550da0539111
zxy <3102194154@qq.com>
2021-08-01 16:41:20 +0800

    删除没用为文件

--- component/tabbar/index.js
+++ component/tabbar/index.js
@@ -4,17 +4,17 @@ Component({
     color: "#cdcdcd",
     selectedColor: "#2c2c2c",
     list: [{
-      pagePath: "/pages/mainpage/mainpage",
+      pagePath: "/pages/mainpage/information",
       iconPath: "/image/mainpage/information.png",
       selectedIconPath: "/image/mainpage/information_selected.png",
       text: "数据"
     }, {
-      pagePath: "/pages/mainpage/mainpage",
+      pagePath: "/pages/mainpage/machine/machine",
       iconPath: "/image/mainpage/account.png",
       selectedIconPath: "/image/mainpage/account_selected.png",
       text: "机器"
     },{
-      pagePath: "/pages/mainpage/mainpage",
+      pagePath: "/pages/mainpage/customer_service/customer_service",
       iconPath: "/image/mainpage/customer_service.png",
       selectedIconPath: "/image/mainpage/customer_service_selected.png",
       text: "客服"
@@ -31,6 +31,6 @@ Component({
         this.setData({
           selected: data.index
         })
-    }
+    },
   }
 })
