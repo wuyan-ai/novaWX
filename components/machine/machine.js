548849eb52bbe0404b902ee6bc2941d485c5747a
zxy <3102194154@qq.com>
2021-08-30 21:47:35 +0800

    功能合并完成

--- components/machine/machine.js
+++ components/machine/machine.js
@@ -25,9 +25,10 @@ Component({
         show:'true',
         left: '10%',
         right:'5%',
-        top: '5%', 
+        top: '8%', 
         width: '85%', 
-        height: '85%'
+        height: '80%',
+        backgroundColor: 'rgb(250,250,250)',
       },
       tooltip: {
         show: true,
@@ -94,12 +95,18 @@ Component({
         type: 'category',
         boundaryGap: false,
         data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+        splitLine: {
+          show: true,
+          lineStyle:{
+               type: 'solid'
+          }
+      },
       },
       yAxis: {
         type: 'value',
         splitLine: {
           lineStyle: {
-            type: 'dashed'
+            type: 'solid'
           }
         }
       }
@@ -158,15 +165,55 @@ Component({
               month:app.data.globalDate.getMonth()+1
             }
             var option = {
+              tooltip:{
+                show: true,
+                trigger: 'axis',
+                axisPointer:{
+                    type: 'cross',
+                    axis: 'y',
+                    crossStyle:{
+                        type: 'solid'
+                    }
+                }
+            },
               xAxis: {
                 type: 'category',
                 boundaryGap: false,
                 data: app.getLineXaisLabel(tempData),
+                splitLine: {
+                  lineStyle: {
+                    type: 'solid',
+                  }
+                },
+                data: xAxisData,
               },
               series: [{
-                symbol: 'none',
+                showSymbol: false,        //设置点击显示小圆点
                 type: 'line',
                 smooth: true,
+                itemStyle : {
+                  normal : {
+                    color: "rgb(220,144,150)",     //设置小圆点颜色
+                    lineStyle:{
+                      color:'rgb(220,144,150)'
+                    }
+                  }
+                },
+                areaStyle:{
+                  normal:{
+                      //右，下，左，上
+                      color:new echarts.graphic.LinearGradient(0,0,0,1,[
+                          {
+                              offset:0,
+                              color:'rgb(220,144,150)'
+                          },
+                          {
+                              offset:1,
+                              color:'rgb(250,250,250)'
+                          }
+                      ],false) 
+                  }
+              },
                 data: res.data.data}]}
                 that.data.chart.setOption(option);
           }
